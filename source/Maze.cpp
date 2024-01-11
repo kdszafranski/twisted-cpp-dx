67bc3464c74f26a7c7f71ac5ce4162038caf10ae
Kris Szafranski <kris@thoughtshelter.com>
2024-01-11 13:20:38 -0600

    working thru stack and container woes

--- source/Maze.cpp
+++ source/Maze.cpp
@@ -17,6 +17,8 @@ Maze::Maze()
 		// fill new vector
 		for (int j = 0; j < width; j++) {
 			Cell cell;
+			cell.myX = i;
+			cell.myY = j;
 			vec.push_back(cell);
 		}
 		// add filled vector to outer vector
@@ -29,21 +31,52 @@ Maze::~Maze()
 	cells.clear();
 }
 
+/*
+Choose the initial cell, mark it as visited and push it to the stack
+While the stack is not empty
+	Pop a cell from the stack and make it a current cell
+	If the current cell has any neighbours which have not been visited
+	Push the current cell to the stack
+	Choose one of the unvisited neighbours
+	Remove the wall between the current cell and the chosen cell
+	Mark the chosen cell as visited and push it to the stack
+*/
 void Maze::Generate()
 {
-	Cell *currentCell; // ref only so we can change the source struct
+	Cell someCell = GetCell(5, 6);
+	checkCells.push_back(someCell);
 
-	for (int i = 0; i < height; i++) {
-		// cols		
-		for (int j = 0; j < width; j++) {
-			currentCell = &cells.at(i).at(j);
+	while (checkCells.size() > 0) {
+		Cell* currentCell = &checkCells.back();
 
-			// every other cell
-			if (j % 2 == 0) {
-				currentCell->bVisited = true;
+		// check right
+		Cell* visit = GetCell(currentCell->myX + 1, currentCell->myY);
+		if (visit) {
+			if (visit->bVisited == false) {
+
 			}
-
 		}
 	}
+
+	//for (int i = 0; i < height; i++) {
+	//	// cols		
+	//	for (int j = 0; j < width; j++) {
+	//		currentCell = &cells.at(i).at(j);
+	//		
+
+
+	//	}
+	//}
+}
+
+void Maze::VisitCell(Cell *cell)
+{
+	cell->bVisited = true;
+
+}
+
+Cell Maze::GetCell(int x, int y)
+{
+	return cells.at(x).at(y);
 }
 
--- source/Maze.h
+++ source/Maze.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>;
 
+
 /*
 
 1. get cells displayed in uniform grid
@@ -13,6 +14,8 @@ using namespace std;
 
 struct Cell
 {
+	int myX;
+	int myY;
 	bool bVisited = false;
 };
 
@@ -22,11 +25,14 @@ public:
 	unsigned int height;
 	unsigned int width;
 	vector<vector<Cell>> cells;
+	vector<Cell> checkCells;
 
 public:
 	Maze();
 	~Maze();
 
 	void Generate();
+	void VisitCell(Cell *cell);
+	Cell GetCell(int x, int y);
 };
 
